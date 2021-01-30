14bb3bd30d89e32f4207bc166ac74385ba7f4f70
kanishka424 <kanishkamalpana24@gmail.com>
2021-01-30 13:30:37 +0530

    react hooks with useState with previous state

---
+++ src/components/ClassCounter.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class ClassCounter extends Component {
+    constructor(props){
+        super(props)
+            this.state={
+                count:0
+
+            }
+        
+    }
+
+     IncrementCount = () =>{
+        this.setState({count:this.state.count+1});
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.IncrementCount}>Count is {this.state.count}</button>
+            </div>
+        )
+    }
+}
+
+export default ClassCounter
